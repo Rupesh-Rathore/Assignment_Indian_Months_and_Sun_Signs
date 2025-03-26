3608157ca3214a2cc02f333dda03e4cfa3ddcf18
Rupesh <rupesh010903@gmail.com>
2025-03-26 23:57:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("months").onclick = function () {
+    window.location.href = "months.html";}
+document.getElementById("zodiacs").onclick = function () {
+        window.location.href = "zodiac.html";}
